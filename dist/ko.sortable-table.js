39b8fcf8ee3621c327057ddf02350957a059f598
Ryan French <rfrench@lic.co.nz>
2015-01-05 13:54:30 +1300

    Throw an error if incorrect arguments are passed to the binding handler

--- dist/ko.sortable-table.js
+++ dist/ko.sortable-table.js
@@ -3,7 +3,12 @@ ko.bindingHandlers.sortBy = {
         var c = !0;
         a.style.cursor = "pointer", a.onclick = function() {
             var a = b(), d = a.array, e = a.sortBy;
-            if (c = !c, "function" == typeof e) d.sort(e); else if (Array.isArray(e)) {
+            switch (c = !c, !0) {
+              case "function" == typeof e:
+                d.sort(e);
+                break;
+
+              case Array.isArray(e):
                 var f = e.length;
                 d.sort(function(a, b) {
                     for (var c = -1; ++c < f; ) {
@@ -11,9 +16,17 @@ ko.bindingHandlers.sortBy = {
                         if (a[d] != b[d]) return a[d] > b[d] ? 1 : -1;
                     }
                 });
-            } else "string" == typeof e && d.sort(function(a, b) {
-                return a[e] > b[e] ? 1 : a[e] < b[e] ? -1 : 0;
-            });
+                break;
+
+              case "string" == typeof e:
+                d.sort(function(a, b) {
+                    return a[e] > b[e] ? 1 : a[e] < b[e] ? -1 : 0;
+                });
+                break;
+
+              default:
+                throw "Incorrect argument for sortBy";
+            }
         };
     }
 };
--- src/ko.sortable-table.js
+++ src/ko.sortable-table.js
@@ -8,28 +8,34 @@ ko.bindingHandlers.sortBy = {
       var data = value.array;
       var sortBy = value.sortBy;
 
+      if(!(ko.isObservable(data) && !Array.isArray(data())) throw "Incorrect argument for array. Array must be an observableArray";
+
       asc = !asc;
 
-      if(typeof sortBy === "function"){
-        data.sort(sortBy);
-      }
-      else if(Array.isArray(sortBy)){
-        console.log(sortBy);
-        var length = sortBy.length;
-        data.sort(function(a,b){
-          var index = -1;
-          while(++index < length)
-          {
-            var field = sortBy[index];
-            if(a[field] == b[field]) continue;
-            return a[field] > b[field] ? 1 : -1;
-          }
-        })
-      }
-      else if(typeof sortBy === "string"){
-        data.sort(function(a,b){
-          return a[sortBy] > b[sortBy] ? 1 : a[sortBy] < b[sortBy] ? -1 : 0;
-        })
+      switch(true) {
+        case typeof sortBy === "function":
+          data.sort(sortBy);
+          break;
+        case Array.isArray(sortBy):
+          var length = sortBy.length;
+          data.sort(function(a,b){
+            var index = -1;
+            while(++index < length)
+            {
+              var field = sortBy[index];
+              if(a[field] == b[field]) continue;
+              return a[field] > b[field] ? 1 : -1;
+            }
+          })
+          break;
+        case typeof sortBy === "string":
+          data.sort(function(a,b){
+            return a[sortBy] > b[sortBy] ? 1 : a[sortBy] < b[sortBy] ? -1 : 0;
+          })
+          break;
+        default:
+          throw "Incorrect argument for sortBy";
+          break;
       }
     };
   }
